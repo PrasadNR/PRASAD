8c1d2f031509a0c195a1172a0a67a3f24a2a9ad9
Prasad N R <PrasadNR@users.noreply.github.com>
2023-12-03 21:04:50 +0530

    There is some practical problem

--- torch_autograd.py
+++ torch_autograd.py
@@ -13,20 +13,21 @@ dict_net["layers"]["layer1"] = (256, 784)  # (784, 256)
 
 dict_net["ReLU"] = dict()
 dict_net["ReLU"]["layer2"] = False
-dict_net["ReLU"]["layer2"] = True
+dict_net["ReLU"]["layer1"] = True
 
 dict_net = init_weights(dict_net)
 
-for i in range(train_targets.shape[0]):
-    x = train_data[i, :]
-    y = train_targets[i]
+n_epochs = 8; t0 = time()
+for j in range(n_epochs):
+    for i in range(train_targets.shape[0]):
+        x = train_data[i, :]
+        y = train_targets[i]
 
-    ASAD_output = forward(x, dict_net)
-    loss_criterion = nn.CrossEntropyLoss()
-    loss = loss_criterion(ASAD_output, y)
-    loss.backward()
+        ASAD_output = forward(x, dict_net)
+        loss_criterion = nn.CrossEntropyLoss()
+        loss = loss_criterion(ASAD_output, y)
+        loss.backward()
 
-    dict_net = update_weights(dict_net, LR=0.001)
+        dict_net = update_weights(dict_net, LR=0.001)
 
-    print(loss)
-
+print(time() - t0, "seconds")
