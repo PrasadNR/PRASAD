c097642057e37e58d8a2ecbb8d83655584b3a67e
Prasad N R <PrasadNR@users.noreply.github.com>
2023-12-03 20:36:56 +0530

    Run this

--- torch_autograd.py
+++ torch_autograd.py
@@ -1,19 +1,32 @@
 import torch
+from time import time
 import torch.nn as nn
+from PRASAD import data_loader, forward, init_weights, update_weights
 
-x = torch.ones(4)
-w = torch.randn(4, requires_grad=True)
+train_data, train_targets, test_data, test_targets = data_loader()
 
-LR = 0.1; loss = 0
-print(x, w)
+dict_net = dict()
 
-for i in range(100):
-    print(loss)
+dict_net["layers"] = dict()
+dict_net["layers"]["layer2"] = (10, 256)  # (256, 10)
+dict_net["layers"]["layer1"] = (256, 784)  # (784, 256)
 
-    output = torch.prod(x + w)
-    loss = output ** 2
+dict_net["ReLU"] = dict()
+dict_net["ReLU"]["layer2"] = False
+dict_net["ReLU"]["layer2"] = True
+
+dict_net = init_weights(dict_net)
+
+for i in range(train_targets.shape[0]):
+    x = train_data[i, :]
+    y = train_targets[i]
+
+    ASAD_output = forward(x, dict_net)
+    loss_criterion = nn.CrossEntropyLoss()
+    loss = loss_criterion(ASAD_output, y)
     loss.backward()
 
-    w = nn.Parameter(torch.Tensor(w.detach() - LR * w.grad.detach()))
+    dict_net = update_weights(dict_net, LR=0.001)
 
-print(w)
+    print(loss)
+
