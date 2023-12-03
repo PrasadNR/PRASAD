32467dd70e163307c47f22f9d835d778b74439ad
Prasad N R <PrasadNR@users.noreply.github.com>
2023-12-03 13:22:43 +0530

    Vector version

--- torch_autograd.py
+++ torch_autograd.py
@@ -1,27 +1,19 @@
 import torch
 import torch.nn as nn
 
-x1 = torch.ones(1)
-x2 = torch.ones(1)
-x3 = torch.ones(1)
-x4 = torch.ones(1)
+x = torch.ones(4)
+w = torch.randn(4, requires_grad=True)
 
-w1 = torch.randn(1, requires_grad=True)
-w2 = torch.randn(1, requires_grad=True)
-w3 = torch.randn(1, requires_grad=True)
-w4 = torch.randn(1, requires_grad=True)
-
-LR = 0.001; loss = 0
+LR = 0.1; loss = 0
+print(x, w)
 
 for i in range(100):
     print(loss)
 
-    output = (x1 + w1) * (x2 + w2) * (x3 + w3) * (x4 + w4)
+    output = torch.prod(x + w)
     loss = output ** 2
     loss.backward()
 
-    w1 = nn.Parameter(torch.Tensor(w1.detach() - LR * w1.grad.detach()))
-    w2 = nn.Parameter(torch.Tensor(w2.detach() - LR * w2.grad.detach()))
-    w3 = nn.Parameter(torch.Tensor(w3.detach() - LR * w3.grad.detach()))
-    w4 = nn.Parameter(torch.Tensor(w4.detach() - LR * w4.grad.detach()))
+    w = nn.Parameter(torch.Tensor(w.detach() - LR * w.grad.detach()))
 
+print(w)
