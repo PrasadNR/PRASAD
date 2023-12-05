3db9cd16ed0566fdcab9a7efd21a3813b1c0d00a
Prasad N R <PrasadNR@users.noreply.github.com>
2023-12-06 03:13:56 +0530

    Layer 1 output Blind Descent

--- PRASAD_Blind_Descent.py
+++ PRASAD_Blind_Descent.py
@@ -1,7 +1,9 @@
 import numpy as np
+from time import time
 from sklearn.datasets import load_digits
 from sklearn.model_selection import train_test_split
 
+t0 = time()
 X_train, X_test, y_train, y_test = train_test_split(*load_digits(return_X_y=True))
 print(X_train.shape, X_test.shape, y_train.shape, y_test.shape)
 
@@ -11,8 +13,11 @@ dict_net["layer1"] = np.random.uniform(-1, 1, size=(64, 32))
 
 X_train_3d = np.repeat(X_train[:, :, None], dict_net["layer1"].shape[1], axis=2)
 layer1_weights_3d = np.repeat(dict_net["layer1"][None, :, :], N_TRAIN_ROWS, axis=0)
-
-
-print(X_train_3d.shape, layer1_weights_3d.shape)
 layer1_output = X_train_3d + layer1_weights_3d
-print(layer1_output)
+layer1_prod_output = np.prod(layer1_output, axis=1)
+layer1_relu_output = np.copy(layer1_prod_output)
+layer1_relu_output[layer1_relu_output < 0] = 0
+
+
+
+print(time() - t0, "seconds")
